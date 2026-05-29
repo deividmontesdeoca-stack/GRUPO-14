7248726e34519aeb5224f8fed4830ca1e48cb0c0
kailarodriguez <kaila.rodriguez@estudiantes.utec.edu.uy>
2026-05-29 18:53:45 -0300

    División por cero. Kaila

---
+++ src/DivisionPorCero_01.java
@@ -0,0 +1,20 @@
+public class DivisionPorCero_01 {
+    public static void main(String[] args) {
+
+        try {
+            int dividendo = 10;
+            int divisor = 0;
+
+            int cuenta = dividendo / divisor;
+
+            System.out.println("Resultado de la división: " + cuenta);
+
+        } catch (ArithmeticException errorMatematico) {
+            System.out.println("No es posible dividir entre cero. Ingrese un número válido.");
+
+        } finally {
+            System.out.println("Proceso de división terminado.");
+        }
+
+    }
+}
---
+++ src/DivisionPorCero_02.java
@@ -0,0 +1,11 @@
+public class DivisionPorCero_02 {
+    public static void main(String[] args) {
+
+        //Código que podría lanzar una excepción
+
+
+
+        //Captura la excepción
+
+    }
+}
