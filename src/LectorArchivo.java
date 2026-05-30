a255211e94dddec04b6b7d8d45ec7cd6b936a566
DeividMdoo <dmontesdeocaacosta@gmail.com>
2026-05-29 21:04:21 -0300

    AgregueLector mas explicacion

--- src/LectorArchivo.java
+++ src/LectorArchivo.java
@@ -1,10 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+//Importamos.
 
 public class LectorArchivo {
     public static void main(String[] args) {
-        leerArchivo();
+        leerArchivo(); //Para poder mostrar en esta misma clase.
     }
 
 
@@ -12,16 +13,16 @@ public class LectorArchivo {
 
             try {
                 File archivo = new File("datos.txt");
-                Scanner sc = new Scanner(archivo);
+                 Scanner sc = new Scanner(archivo); // creamos un scanner que puede leer un archivo.
 
-                while (sc.hasNextLine()) {
-                    System.out.println(sc.nextLine());
+                while (sc.hasNextLine()) { // Mientras tenga contenido.
+                    System.out.println(sc.nextLine()); // se muestra o lee.
                 }
 
-                sc.close();
+                sc.close(); // cierra el scanner
 
             } catch (FileNotFoundException e) {
-                System.out.println("El archivo no existe");
+                System.out.println("El archivo no existe"); // si da error muestra lo siguinte
             }
         }
     }
